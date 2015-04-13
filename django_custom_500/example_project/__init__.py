351bd70769b95aa5c530a621fd25f0f73bdf7d10
Vašek Dohnal <vaclav.dohnal@gmail.com>
2015-04-13 12:03:10 +0200

    Fix test for Django==1.6.0; Added tox

--- django_custom_500/example_project/__init__.py
+++ django_custom_500/example_project/__init__.py
@@ -9,9 +9,9 @@ import sys
 import os
 
 if "nosetests" in sys.argv[0]:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_custom_500.example_project.settings")
+
     from django.core.wsgi import get_wsgi_application
 
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_custom_500.example_project.settings")
 
     application = get_wsgi_application()
