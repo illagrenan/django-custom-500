9204a486d2aaf81841d37f246255ec4655384d56
Vašek Dohnal <vaclav.dohnal@gmail.com>
2015-10-21 11:10:32 +0200

    Editor and git config. Tests rename.

--- django_custom_500/example_project/tests/test_basic.py
+++ django_custom_500/example_project/tests/test_basic.py
@@ -14,19 +14,19 @@ import requests
 from django.test import TestCase, LiveServerTestCase
 
 
-class RenderToTestCase(TestCase):
-    def test_content_type_kwarg(self):
-        response = self.client.get('/normal-view-that-returns-data')
+class NormalViewTestCase(TestCase):
+    def test_normal_view(self):
+        ok_response = self.client.get('/normal-view-that-returns-data')
 
-        self.assertTrue('42' in response.content)
+        self.assertTrue('42' in ok_response.content)
 
 
-class ACase(LiveServerTestCase):
-    def test_content_asdasrg(self):
-        abc = requests.get('%s%s' % (self.live_server_url, '/view-that-raises-value-error'))
+class InternalErrorTestCase(LiveServerTestCase):
+    def test_error_view(self):
+        error_response = requests.get('%s%s' % (self.live_server_url, '/view-that-raises-value-error'))
 
-        self.assertEqual(abc.status_code, 500)
-        self.assertIn("500 Internal Server Error", abc.content)
+        self.assertEqual(error_response.status_code, 500)
+        self.assertIn("500 Internal Server Error", error_response.content)
 
 
 if __name__ == "__main__":
